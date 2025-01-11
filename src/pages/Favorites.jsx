70128f91ae37e6ac40588554869a2a9f0ee0d6a5
MConnors26 <mpconnors026@gmail.com>
2025-01-11 08:32:14 -0500

    Favorites.jsx

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No favorite fish yet</h2>
+        <p>Start adding fish to your favorites</p>
+    </div>
+}
+
+export default Favorites
